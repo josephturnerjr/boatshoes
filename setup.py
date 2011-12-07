584691704d8edee2685d31620201a155dd0d18f6
Joseph Turner <turner@miserware.com>
2011-12-06 19:33:44 -0500

    Added the setup and the gitignore

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='crindy',
+      version='1.0',
+      description='Crindy: dead-simple eventloop programming for Python',
+      author='Joseph Turner',
+      author_email='turner@miserware.com',
+      url='https://github.com/josephturnerjr/crindy',
+      packages=['boatshoes'],
+     )
